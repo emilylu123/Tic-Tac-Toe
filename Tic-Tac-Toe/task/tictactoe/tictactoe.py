8c6053f908239ca2269b979f7275cf5e64a2e0d1
Emily Lu <emilylu123@gmail.com>
2021-01-18 15:02:41 +1030

    python game project tictactoe

---
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -0,0 +1,10 @@
+# write your code here
+
+cell = input('Enter cells:')
+out = list(cell)
+
+print('---------')
+print('| ' + out[0] + ' ' + out[1] + ' ' + out[2] + ' |')
+print('| ' + out[3] + ' ' + out[4] + ' ' + out[5] + ' |')
+print('| ' + out[6] + ' ' + out[7] + ' ' + out[8] + ' |')
+print('---------')
